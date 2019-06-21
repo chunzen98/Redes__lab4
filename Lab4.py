fa4a0a78d18bd50a0110ef88bf910cfabaf671de
unknown <chunzen1998@gmail.com>
2019-06-21 00:29:16 -0400

    final

--- Lab4.py
+++ Lab4.py
@@ -1,136 +1,208 @@
-    # --------- IMPORTACIONES ---------
+# --------- IMPORTACIONES ---------
 import scipy.io
 from scipy import interpolate
 from scipy.io import wavfile
-from scipy.fftpack import fft,fftfreq,ifft
+from scipy import signal as sig #Espectograma
+from scipy.fftpack import fft,fftfreq
+from scipy.signal import filtfilt
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ---------- BLOQUE FUNCIONES -------------
 
-# --------- FUNCIONES ---------
-
-def graph_portadora(portadora,rate):
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('f(t)')
-    Tiempo = np.linspace(0, len(portadora) / rate, num=len(portadora))
-    plt.plot(Tiempo, portadora)
-    plt.xlim(0,0.015)
-    plt.show()
-
-def GraficoAM(signal,frecuencia,audio, filename):
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('f(t)')
-    Tiempo = np.linspace(0, len(audio) / frecuencia, num=len(audio))
-    plt.plot(Tiempo, audio, 'g', label = "Senal audio "+filename)
-    plt.plot(Tiempo, signal, label = "Senal modulada")
-    plt.legend()
-    plt.xlim(0, 0.1)
-    plt.show()
-
-def graph_AM(signal,rate,data,portadora,filename):
-    plt.ion()
-    plt.figure("Portadora")
-    graph_portadora(portadora,rate)
-    plt.pause(0.0001)
-    plt.figure("Modulacion AM")
-    GraficoAM(signal, rate, data, filename)
-    plt.pause(0.0001)
-    plt.show(block=True)
-
+##Recibe la senal y su frecuencia de muestreo, interpola los valores de la funcion, retorna la senal interpolada
 def interpolacionAM(signal,frecuencia):
-    Tiempo=np.linspace(0, len(signal)/frecuencia, num=len(signal))
-    interpolada = interpolate.interp1d(Tiempo, signal)
-    Tiempo2 = np.linspace(0, len(signal)/frecuencia, len(signal)*4)
-    y2 = interpolada(Tiempo2)
+    tiempo = np.linspace(0, len(signal)/frecuencia, num=len(signal))
+    interpolada = interpolate.interp1d(tiempo, signal)
+    tiempo2 = np.linspace(0, len(signal)/frecuencia, num=len(signal) * 4)
+    y2 = interpolada(tiempo)
     return y2
 
-
-def modulation_am_time():
-    rate,data=wavfile.read("handel.wav")
-    signal_interp_AM = interpolacionAM(data,rate)
-    largo_AM = len(signal_interp_AM)
-    tiempo_AM = np.linspace(0, largo_AM/rate, num = largo_AM)
-    portadora = np.cos(2*np.pi*27000000*tiempo_AM)
-    y = signal_interp_AM * portadora
-    graph_AM(y,rate,signal_interp_AM,portadora,"handel.wav")
-
-
-# --------- BLOQUE PRINCIPAL ---------
+##Recibe la senal, su frecuencia y el coeficiente de modulacion. Modula la senal en am segun la senal portadora
+##portadora y el coeficiente dado. Retorna la senal modulada    
+def modulacionAM(signal,frecuencia,interpolada,coeficiente):
+    #Obtencion de senales portadora y modulada
+    largoInterpolada = len(interpolada)
+    fsAM = frecuencia * 3
+    tiempoModulada = np.linspace(0,largoInterpolada/frecuencia,num=largoInterpolada)
+    tiempo = np.linspace(0,len(signal)/frecuencia,num=len(signal))
+    senalPortadora = np.cos(2*np.pi*fsAM*tiempoModulada)
+    senalModulada =  interpolada*senalPortadora*coeficiente
 
 
-# 1)
-# --------- DATOS DE LA SENAL ---------
+    plt.figure("Mod. AM")
+    #Graficos de senales
+    plt.subplot(311)
+    plt.plot(tiempo,signal,"c")
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Original")
+    plt.title("Señal Original en el tiempo")
+    
+    tiempoPortadora = np.linspace(0,len(senalPortadora)/frecuencia,num=len(senalPortadora))
 
-# Importacion de los datos de la senal
+    plt.subplot(312)
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Portadora")
+    plt.title("Portadora en el tiempo")
+    plt.plot(tiempoPortadora,senalPortadora)
+
+    plt.subplot(313)
+    plt.plot(tiempoModulada,senalModulada, "r")
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Modulada")
+    plt.title("Señal Modulada en el tiempo")
+
+    plt.suptitle("Modulacion AM", fontsize=16)
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.88)
+
+    return senalModulada, fsAM
+
+
+def modulacionFM(signal,frecuencia,interpolada,coeficiente):
+    largoInterpolada = len(interpolada)
+    fsFM = frecuencia * 3
+    tiempoModulada = np.linspace(0,largoInterpolada/frecuencia,num=largoInterpolada)
+    tiempo = np.linspace(0,len(signal)/frecuencia,num=len(signal))
+    senalPortadora = np.cos(2*np.pi*fsFM*tiempoModulada)
+    senalModulada = np.cos(2*np.pi*fsFM*tiempoModulada + (np.cumsum(interpolada)/frecuencia)*coeficiente)
+
+
+    plt.figure("Mod. FM")
+    plt.subplot(311)
+    plt.plot(tiempo,signal,"c")
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Original")
+    plt.title("Señal Original en el tiempo")
+
+    plt.subplot(312)
+    tiempoPortadora = np.linspace(0,len(senalPortadora)/frecuencia, num=len(senalPortadora))
+    plt.plot(tiempoPortadora,senalPortadora)
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Portadora")
+    plt.title("Portadora en el tiempo ")
+    
+    
+    plt.subplot(313)
+    plt.plot(tiempoModulada,senalModulada,"r")
+    plt.xlabel("Tiempo(s)")
+    plt.ylabel("Modulada")
+    plt.title("Señal Modulada en el tiempo fm")
+    
+    plt.suptitle("Modulacion FM", fontsize=16)
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.88)
+
+    return senalModulada, fsFM
+
+
+def transFourier(audio, AM, FM, fs, fsAM, fsFM):
+
+    largo = len(audio)
+    
+    dt = 1 / float(fs)
+    dtAM = 1 / float(fsAM)
+    dtFM = 1 / float(fsFM)
+    
+    yFourier = abs(fft(audio) / largo) #valor abs
+
+    yAMFourier = abs(fft(AM) / largo) #valor abs
+
+    yFMFourier = abs(fft(FM) / largo) #valor abs
+
+    fsFourier = fftfreq(int(largo), dt)
+    fsAMFourier = fftfreq(int(largo), dtAM)
+    fsFMFourier = fftfreq(int(largo), dtFM)
+
+
+    plt.figure("Transformada de fourier")
+
+    plt.subplot(311)
+    plt.plot(fsFourier, yFourier, "r")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("Amplitud [dB]")
+    plt.title("Transformada de Fourier original")
+##    plt.text(2, 0.65, "Ancho de banda: " + str(max(fsFourier)))
+
+    plt.subplot(312)
+    plt.plot(fsAMFourier, yAMFourier, "b")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("Amplitud [dB]")
+    plt.title("Transformada de Fourier AM")
+##    plt.text(2, 0.65, "Ancho de banda: " + str(max(fsAMFourier)))
+
+    plt.subplot(313)
+    plt.plot(fsFMFourier, yFMFourier, "c")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("Amplitud [dB]")
+    plt.title("Transformada de Fourier FM")
+##    plt.text(2, 0.65, "Ancho de banda: " + str(max(fsFMFourier)))
+
+    plt.suptitle("Transformada de Fourier de Modulacion", fontsize=16)
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.88)
+
+    print("Ancho de banda Original: " + str(max(fsFourier)))
+    print("Ancho de banda AM: " + str(max(fsAMFourier)))
+    print("Ancho de banda FM: " + str(max(fsFMFourier)))
+        
+    return yFourier, yAMFourier, yFMFourier
+
+
+def demodulacionAM(signal,frecuencia,interpolada):
+    #Obtencion de senales portadora y modulada
+    largoInterpolada = len(interpolada)
+    fsAM = frecuencia * 3
+    tiempoModulada = np.linspace(0,largoInterpolada/frecuencia,num=largoInterpolada)
+    tiempo = np.linspace(0,len(signal)/frecuencia,num=len(signal))
+    senalPortadora = np.cos(2*np.pi*fsAM*tiempoModulada)
+    senalDemodulada = signal * senalPortadora
+
+    #Filtro pasa bajo
+    b, a = sig.butter(3,4000,"low",fs=frecuencia)
+    demodulada = filtfilt(b, a,audio)
+    
+    return senalDemodulada
+
+# ---------- BLOQUE PRINCIPAL -------------
+
+#Obtencion de datos de senal
 fs,audio = wavfile.read("handel.wav")
-
-# Largo de la senal
-largo = float(len(audio))
-# Tiempo de la senal
-t = largo / fs
-dt = 1 / float(fs)
-# Generar arreglo
-tArreglo = np.linspace(0, t, int(largo))
-
-# Muestra por pantalla de datos de senal
-print("Datos de senal original")
-print("frecuencia: ",fs)
-print("tiempo: ",t)
-
-### 2)
-### --------- GRAFICO DE SENAL ---------
-##plt.figure(1)
-##plt.plot(tArreglo, audio, "b")
-##plt.xlabel("Tiempo [s]")
-##plt.ylabel("Amplitud [dB]")
-##plt.title("Grafico en el dominio del tiempo", )
-##
-##
-### 3)
-### --------- TRANSFORMADA DE FOURIER ---------
-##audioFourier = fft(audio)
-##yFourier = audioFourier / largo
-##yFourierPos = abs(fft(audio) / largo) #valor abs
-##fsFourier = fftfreq(int(largo), dt)
-##
-### 3.a)
-### Grafico transformada de Fourier
-##plt.figure(2)
-##plt.plot(fsFourier, yFourierPos, "r")
-##plt.xlabel("Frecuencia [Hz]")
-##plt.ylabel("Amplitud [dB]")
-##plt.title("Transformada de Fourier grafico")
-
-# --------- ESCRITURA ARCHIVO .WAV ---------
-# Ajuste datos para que sean aceptadas en la funcion wavfile.write
-#fourierInvFiltro = np.asarray(yFourierInvFiltro, dtype=np.int16)
-
-print("Escribiendo audios...")
-#wavfile.write("FourierInv.wav", fs, fourierInv)
-#wavfile.write("FourierInvFiltro.wav", fs, fourierInvFiltro)
-print("Audio .wav escrito exitosamente.")
+largo = float(len(audio)) # Largo de la senal
+t = largo / fs # Tiempo de la senal
 
 
+#2)
+interpolada = interpolacionAM(audio,fs)
+moduladaAM, fsAM = modulacionAM(audio,fs,interpolada,1)
+moduladaFM, fsFM = modulacionFM(audio,fs,interpolada,1)
+yFourier, yAMFourier, yFMFourier = transFourier(audio, moduladaAM, moduladaFM, fs, fsAM, fsFM)
 
 
-Tiempo = np.linspace(0, len(audio)/fs, num=len(audio))
-interpolada = interpolate.interp1d(Tiempo, audio)
-Tiempo2 = np.linspace(0, len(audio)/fs, len(audio)*4)
-y2 = interpolada(Tiempo2)
+#3)
+# Demodulacion AM
+demoduladaAM = demodulacionAM(moduladaAM, fs, interpolada)
 
+# Grafico demodulacion
+tiempo = np.linspace(0,len(audio)/fs, num=len(audio))
 
+plt.figure("Demodulacion")
 
-#largo_AM = len(signal_interp_AM)
-tiempo_AM = np.linspace(0, len(Tiempo), 1)
-portadora = np.cos(2*np.pi*100*tiempo_AM)
+plt.subplot(211)
+plt.plot(tiempo, audio,"c")
+plt.xlabel("Tiempo(s)")
+plt.ylabel("Amplitud [dB]")
+plt.title("Señal Original en el tiempo")
 
+plt.subplot(212)
+plt.plot(tiempo, demoduladaAM,"r")
+plt.xlabel("Tiempo(s)")
+plt.ylabel("Amplitud [dB]")
+plt.title("Señal Demodulada")    
+    
+plt.suptitle("Demodulación", fontsize=16)
+plt.tight_layout()
+plt.subplots_adjust(top=0.88)
 
-
-print(len(y2))
-
-plt.figure(3)
-plt.plot(tiempo_AM, portadora)
-#plt.xlim(0, 100)
-
-plt.show() # Mostrar todos los graficos
+plt.show()
